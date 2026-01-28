d2e2c121bcc6c4d6c563759ede9aed613aca64a4
Sergei Filippo <933117@gmail.com>
2026-01-28 10:53:10 +0000

    feat: Security policies added, nginx config updated

--- infra/terraform/gke.tf
+++ infra/terraform/gke.tf
@@ -8,6 +8,17 @@ resource "google_container_cluster" "primary" {
   network    = google_compute_network.vpc.id
   subnetwork = google_compute_subnetwork.private.id
 
+  # Enable Network Policy (Calico)
+  network_policy {
+    enabled = true
+  }
+  
+  addons_config {
+    network_policy_config {
+      disabled = false
+    }
+  }
+
   # Enable VPC-native traffic
   ip_allocation_policy {
     cluster_secondary_range_name  = "k8s-pod-range"
